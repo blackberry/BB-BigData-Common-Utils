3905b850afb25b8f8305ad8f8e5457d441bcade6
Dave Ariens <dariens@blackberry.com>
2015-03-17 15:19:16 +0000

    adds jmx

---
+++ src/main/java/com/blackberry/bdp/common/utils/jmx/MetricRegistrySingleton.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2014 BlackBerry, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+package com.blackberry.bdp.common.utils.jmx;
+
+import java.util.concurrent.TimeUnit;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.MetricRegistry;
+
+/**
+ * A singleton to hold a metric registry.
+ */
+public class MetricRegistrySingleton
+{
+	private final MetricRegistry metricRegistry;
+	private JmxReporter jmxReporter;
+	private ConsoleReporter consoleReporter;
+
+	private MetricRegistrySingleton()
+	{
+		metricRegistry = new MetricRegistry();
+	}
+
+	private static class SingletonHolder
+	{
+		private static final MetricRegistrySingleton INSTANCE = new MetricRegistrySingleton();
+	}
+
+	/**
+	 * Get the instance of MetricRegistrySingleton.
+	 *
+	 * @return the instance of the singleton.
+	 */
+	public static MetricRegistrySingleton getInstance()
+	{
+		return SingletonHolder.INSTANCE;
+	}
+
+	/**
+	 * Get the MetricRegistry instance associated with the singleton.
+	 *
+	 * @return the MetricRegistry
+	 */
+	public MetricRegistry getMetricsRegistry()
+	{
+		return metricRegistry;
+	}
+
+	/**
+	 * Turn on JMX reporting for this MetricRegistry.
+	 *
+	 * It is safe to call this multiple times.
+	 */
+	public synchronized void enableJmx()
+	{
+		if (jmxReporter == null)
+		{
+			jmxReporter = JmxReporter.forRegistry(metricRegistry).build();
+			jmxReporter.start();
+		}
+	}
+
+	/**
+	 * Enable console reporting for this MetricRegistry with 1 minute frequency.
+	 */
+	public void enableConsole()
+	{
+		enableConsole(100);
+	}
+
+	/**
+	 * Enable console reporting for this MetricRegistry.
+	 *
+	 * @param ms how often to output data to the console, in milliseconds.
+	 */
+	public synchronized void enableConsole(long ms)
+	{
+		if (consoleReporter == null)
+		{
+			consoleReporter = ConsoleReporter.forRegistry(metricRegistry)
+				 .convertRatesTo(TimeUnit.SECONDS)
+				 .convertDurationsTo(TimeUnit.MILLISECONDS).build();
+			consoleReporter.start(ms, TimeUnit.MILLISECONDS);
+		}
+	}
+}
