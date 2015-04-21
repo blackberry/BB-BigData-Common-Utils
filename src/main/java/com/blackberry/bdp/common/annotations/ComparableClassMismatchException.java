6e6797222faeda7633a8d7c735b5311d56465130
Dave Ariens <dariens@blackberry.com>
2015-04-21 19:28:09 +0000

    initial VersionedComparable interface/annotion

---
+++ src/main/java/com/blackberry/bdp/common/annotations/ComparableClassMismatchException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blackberry.bdp.common.annotations;
+
+/**
+ *
+ * @author dariens
+ */
+public class ComparableClassMismatchException extends Exception {
+	public ComparableClassMismatchException(String message) {
+		super(message);
+	}	
+}
